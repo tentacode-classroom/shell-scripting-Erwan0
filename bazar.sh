1d7b96076b90ceda91a4b0c37e8e55c495ddf0ea
Erwan0 <erwan.guillemin@ynov.com>
2018-12-19 12:54:23 +0100

    a

---
+++ bazar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf ./bazar
+
+mkdir -p ./bazar/Bureau/secret
+mkdir -p ./bazar/Download
+
+touch ./bazar/Bureau/secret/truc.txt
+touch ./bazar/Bureau/secret/ok.txt
+touch ./bazar/Bureau/secret/a.txt
+touch ./bazar/Download/cv.txt
+
+cd ./bazar/Bureau
+
+wget https://fr.cdn.v5.futura-sciences.com/buildsv6/images/mediumoriginal/4/0/b/40b157a070_81942_aurorebo7.jpg -q -O aurore-boreale.jpg
+
+wget https://framboisemood.files.wordpress.com/2014/01/512.jpg -q -O lac.jpg
